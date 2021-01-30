d600a728e837710593343c4e763b31e47d7f1765
Guilhem Sarcy <guilhem.sarcy@gmail.com>
2021-01-30 16:53:41 +0100

    chore(ci):prepare for heroku app

--- init.cmd
+++ init.cmd
@@ -1,7 +1,9 @@
+REM remove existing virtual env
+call conda remove --name cryptolution --all -y
 REM virtual env creation
-call conda create -n cryptolution_prod python=3.7 -y
+call conda create -n cryptolution python=3.7 -y
 REM virtual env activation
 call conda deactivate
-call conda activate cryptolution_prod
+call conda activate cryptolution
 REM requirements installation
 call pip install -r requirements.txt
