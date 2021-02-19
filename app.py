3b26e05e852c6636a45b44bd31fd4b9c428a7912
Guilhem Sarcy <guilhem.sarcy@gmail.com>
2021-02-19 22:58:01 +0100

    chore(ci):fix isort

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import datetime as dt
 import json
-
-import dash
-import dash_core_components as dcc
-import dash_bootstrap_components as dbc
-import dash_html_components as html
-import pandas as pd
 from datetime import timedelta
 
-# Dash App
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
