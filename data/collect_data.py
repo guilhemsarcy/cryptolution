57de85239c0b9dfcb1db82fd3312e2ebdb6c3aa0
Guilhem Sarcy <guilhem.sarcy@gmail.com>
2021-02-20 23:12:44 +0100

    feat:use since API param to append data

--- data/collect_data.py
+++ data/collect_data.py
@@ -1,6 +1,7 @@
 """Script for data collection from Kraken."""
 
 import logging
+import math
 import json
 import os
 import time
@@ -30,7 +31,7 @@ def collect_data(settings=COLLECTION_SETTINGS):
 
     interval_in_minutes = settings['query_period_in_minutes']
     result_ohlc = pd.DataFrame(columns=['asset_pair', 'wsname', 'asset', 'currency',
-                                        'time', 'open_price', 'close_price', 'volume'])
+                                        'time', 'tmsp', 'open_price', 'close_price', 'volume'])
 
     logger.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} : starting to collect data from Kraken")
 
@@ -59,16 +60,21 @@ def collect_data(settings=COLLECTION_SETTINGS):
         asset = items[1]['asset']
         currency = items[1]['currency']
         current_result_ohlc_asset = pd.DataFrame(columns=['asset_pair', 'wsname', 'asset', 'currency',
-                                                          'time', 'open_price', 'close_price', 'volume'])
+                                                          'time', 'tmsp', 'open_price', 'close_price', 'volume'])
+        data = pd.read_csv("data.csv")
+        last_tmsp = data[data.asset_pair == asset_pair]['tmsp'].max()
+        if not math.isnan(last_tmsp):
+            since = last_tmsp + int(interval_in_minutes) * 60
+        else:
+            since = 0
         ret = False
         while not ret:
             try:
                 logger.info(
                     f"{time.strftime('%Y-%m-%d %H:%M:%S')} : trying to collect data for asset pair {asset_pair}")
                 current_query_ohlc = kraken.query_public(
-                    'OHLC', {'pair': asset_pair, 'interval': interval_in_minutes})
+                    'OHLC', {'pair': asset_pair, 'interval': interval_in_minutes, 'since': since})
                 ret = True
-                print(current_query_ohlc)
             except ValueError:
                 logger.error(f"{time.strftime('%Y-%m-%d %H:%M:%S')} : Kraken not available - retry after 5 sec")
                 time.sleep(5)
@@ -77,10 +83,13 @@ def collect_data(settings=COLLECTION_SETTINGS):
         current_data_ohlc_asset = current_query_ohlc['result'][asset_pair]
         for i in range(0, len(current_data_ohlc_asset)):
             temp_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current_data_ohlc_asset[i][0]))  # period
+            temp_tmsp = current_data_ohlc_asset[i][0]  # raw period
+            if temp_tmsp == last_tmsp:
+                continue
             temp_open_price = current_data_ohlc_asset[i][1]  # price at the beginning of the period
             temp_close_price = current_data_ohlc_asset[i][4]  # price at the end of the period
             temp_volume = current_data_ohlc_asset[i][6]  # number of shares traded
-            current_result_ohlc_asset.loc[i] = [asset_pair, wsname, asset, currency, temp_time,
+            current_result_ohlc_asset.loc[i] = [asset_pair, wsname, asset, currency, temp_time, temp_tmsp,
                                                 temp_open_price, temp_close_price, temp_volume]
         logger.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} : data collected for asset pair {asset_pair}")
         logger.info(f"current progress is {str(int(float(k + 1) / float(len(pairs)) * 100))}%")
@@ -89,7 +98,8 @@ def collect_data(settings=COLLECTION_SETTINGS):
 
     logger.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} : No more data to collect from Kraken")
 
-    result_ohlc.to_csv('data.csv', index=False)
+    data = pd.concat([data, result_ohlc])
+    data.to_csv('data.csv', index=False)
 
     with open('pairs.json', 'w') as jsn:
         json.dump(pairs, jsn, sort_keys=True, indent=4)
