470f7aab15fd804b1e9b085208811b75278614f6
Guilhem Sarcy <guilhem.sarcy@gmail.com>
2023-01-21 10:38:06 +0100

    ref:change repo structure

---
+++ modules/models/models.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from typing import Dict
+from modules.data import COLLECTION_SETTINGS
+
+
+class KrakenDataCollector:
+    """
+
+    """
+    def __init__(
+            self,
+            settings: Dict[str, str] = COLLECTION_SETTINGS
+    ):
+        self.settings = settings
+
+
