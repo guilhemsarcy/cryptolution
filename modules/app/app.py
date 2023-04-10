500407919194d5b6f3339fa0eba632bc91805afa
Guilhem Sarcy <guilhem.sarcy@gmail.com>
2023-04-10 17:27:27 +0200

    ref:refacto explorer

--- modules/app/app.py
+++ modules/app/app.py
@@ -5,7 +5,6 @@ Code for streamlit app.
 import json
 from os import getenv
 
-import altair as alt
 import pandas as pd
 import streamlit as st
 from view import CryptolutionView
@@ -49,47 +48,9 @@ asset_options = sidebar["asset_options"]
 asset_disabled = sidebar["asset_disabled"]
 metric_options = sidebar["metric_options"]
 
-st.header('Explore historical data')
-
-displayed_data = data[
-    (data.asset == asset_disabled) &
-    (data.currency == currency_options)
-].copy()
-
-displayed_data['date'] = pd.to_datetime(displayed_data['time'])
-
-brush = alt.selection(type='interval', encodings=['x'])
-line_chart = (
-    alt.Chart(
-        data=displayed_data,
-        title=f"Evolution of {metric_options} for asset {asset_options}",
-    )
-    .mark_line()
-    .encode(
-        x=alt.X("date:T", axis=alt.Axis(title="date", titleColor='#57A44C')),
-        y=alt.Y(metric_options, axis=alt.Axis(title=f'{metric_options} ({currency_options})', titleColor='#57A44C'))
-    )
-).add_selection(
-    brush
+cryptolution_view.explorer(
+    asset_disabled=asset_disabled,
+    currency=currency_options,
+    metric=metric_options,
+    asset_name=asset_options
 )
-
-line = alt.Chart().mark_rule(color='firebrick').encode(
-    y=f'mean({metric_options}):Q',
-    size=alt.SizeValue(3)
-).transform_filter(brush)
-chart = alt.layer(line_chart, line, data=displayed_data)
-
-zoomed_chart = (
-    alt.Chart(
-        data=displayed_data,
-        title="Zoomed",
-    )
-    .mark_line()
-    .encode(
-        x=alt.X("date:T", axis=alt.Axis(title="date", titleColor='#57A44C')),
-        y=alt.Y(metric_options, axis=alt.Axis(title=f'{metric_options} ({currency_options})', titleColor='#57A44C'))
-    )
-).transform_filter(
-    brush
-)
-st.altair_chart(chart & zoomed_chart, theme="streamlit", use_container_width=True)
